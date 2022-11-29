a2725a5a29a16d1bc7be2cc179338b99c822af3d
Charlotte Webster <charlotte.shae.webster@gmail.com>
2022-11-29 16:03:41 -0700

    complete serial.py exercise

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,23 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Initiate Serial instance with start number and save start reference"""
+        self.start = start
+        self.start_reference = start
+        self.called_before = False
+
+    def generate(self):
+        """On first call, returns start, subsequent calls return Serial (start)
+        incremented by one"""
+        if self.called_before == False:
+            self.called_before = True
+            return self.start
+        else:
+            self.start += 1
+            return self.start
+
+    def reset(self):
+        """Resets Serial to start reference"""
+        self.start = self.start_reference
+        self.called_before = False
