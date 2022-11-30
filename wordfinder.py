2710166b1a4bf8abc27b5b3a4a01b71018445db8
Your Full Name <you@youremail.com>
2022-11-29 19:32:32 -0500

    waiting for second code review

--- wordfinder.py
+++ wordfinder.py
@@ -1,6 +1,49 @@
 from random import choice
 
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
-    
+    def __init__(self, file):
+        """Create list of words from file and
+        prints how many words are in the list"""
+        self.file = open(file)
+        self.words = self.populate_words(self.file)
+        print(f"{len(self.words)} words read")
+
+    def populate_words(self, file):
+        """Populates a list with words from a file"""
+        words = []
+        for line in file:
+            fixed_word = line.replace("\n", "")
+            words.append(fixed_word)
+
+        return words
+
+    def random(self):
+        """Returns a random word from instance's list of words"""
+        return choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    """WordFinder that filters out empty lines and comments"""
+
+    def __init__(self, file):
+        """Create a list of words from file but filters out empty lines
+        and comments, and prints how many words are in the list"""
+        super().__init__(file)
+        self.words = self.fix_words()
+        print(f"{len(self.words)} words read")
+
+    def fix_words(self):
+        """Creates a new list that filters out empty strings and strings that
+        start with '#'"""
+        special_words = []
+        for word in self.words:
+            if not word.startswith("#") and not word == "":
+                special_words.append(word)
+        return special_words
+
+
+# "/Users/petrachoir/Desktop/words.txt"
+# "/usr/share/dict/words"
